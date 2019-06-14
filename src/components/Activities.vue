59a69b085b0da1225857b069268352c428052ce2
Hugo Daniel <hugodaniel.icam@gmail.com>
2019-06-14 14:36:52 +0200

    Add new activity form

--- src/components/Activities.vue
+++ src/components/Activities.vue
@@ -1,6 +1,28 @@
 <template>
   <div id="activities">
-    <div v-for="activity in activities" :key="activity.id">
+    <h3>Add a new activity</h3>
+    <form action="" @submit.prevent="addActivity">
+      <label for="activity_name" class="label">Name</label>
+      <input
+        id="activity_name"
+        class="input"
+        autofocus autocomplete="off"
+        placeholder="Type an activity name"
+        v-model="newActivity.name" />
+
+      <label for="activity_description" class="label">Description</label>
+      <input
+        id="activity_description"
+        class="input"
+        autofocus autocomplete="off"
+        placeholder="Year"
+        v-model="newActivity.description"
+      />
+
+      <input type="submit" value="Add Activity"/>
+    </form>
+
+    <div v-for="activity in activities" :key="activity.id" :activity="activity">
      <p>{{activity.name}} : {{activity.description}}</p>
     </div>
   </div>
@@ -16,17 +38,37 @@ export default {
       activities: [],
       name: [],
       description: [],
+      newActivity: [],
       errors: []
     }
   },
   created () {
-    axios.get(process.env.ROOT_API + '/api/v1/activities', { headers: { Authorization: localStorage.token } })
-      .then(response => {
-        this.activities = response.data
-      })
-      .catch(e => {
-        this.errors.push(e)
-      })
+    if (!localStorage.signedIn) {
+      this.$router.replace('/')
+    } else {
+      axios.get(process.env.ROOT_API + '/api/v1/activities', { headers: { Authorization: localStorage.token } })
+        .then(response => {
+          this.activities = response.data
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
+    }
+  },
+
+  methods: {
+    addActivity () {
+      const value = this.newActivity
+      if (!value) {
+        return
+      }
+      axios.post(process.env.ROOT_API + '/api/v1/activities/', { activity: { name: this.newActivity.name, description: this.newActivity.description } }, { headers: { Authorization: localStorage.token } })
+        .then(response => {
+          this.activities.push(response.data)
+          this.newActivity = ''
+        })
+        .catch(error => this.errors.push(error))
+    }
   }
 }
 </script>
